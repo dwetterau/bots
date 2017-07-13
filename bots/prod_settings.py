9b94f99f002ef3bd11e26b368c0ef2a888da3e2e
David Wetterau <david.wetterau@gmail.com>
2017-07-12 23:08:21 -0700

    revert typo

--- bots/prod_settings.py
+++ bots/prod_settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'bots.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(PROJECTdj_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
     }
 }
 
